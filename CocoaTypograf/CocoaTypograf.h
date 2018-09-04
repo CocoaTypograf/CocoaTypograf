c8cdd13e86891b4b3f797665d94549ad69aee151
Vadim Zhilinkov <dreadct@gmail.com>
2018-09-04 20:30:44 +0700

    refs #1 Setup Xcode project
    Setup project build settings and targets.

---
+++ CocoaTypograf/CocoaTypograf.h
@@ -0,0 +1,19 @@
+//
+//  CocoaTypograf.h
+//  CocoaTypograf
+//
+//  Created by Vadim Zhilinkov on 04/09/2018.
+//  Copyright © 2018 dreadct. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaTypograf.
+FOUNDATION_EXPORT double CocoaTypografVersionNumber;
+
+//! Project version string for CocoaTypograf.
+FOUNDATION_EXPORT const unsigned char CocoaTypografVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTypograf/PublicHeader.h>
+
+
