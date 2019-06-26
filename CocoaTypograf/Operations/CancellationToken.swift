f1675794dd812eb44f8c443520da11ada1914425
Vadim Zhilinkov <dreadct@gmail.com>
2019-06-26 21:26:38 +0700

    refs #22 Add documentation comments to most of the code

--- CocoaTypograf/Operations/CancellationToken.swift
+++ CocoaTypograf/Operations/CancellationToken.swift
@@ -8,12 +8,15 @@
 
 import Foundation
 
+/// Represents a token for cancelling asynchronous operations.
 public struct CancellationToken {
 
     // MARK: - Properties
 
+    /// An associated cancellation closure.
     private let cancellation: () -> Void
 
+    /// Indicates whether the token was already cancelled.
     private(set) var isCancelled: Bool = false {
         didSet {
             if !oldValue && isCancelled {
@@ -24,12 +27,17 @@ public struct CancellationToken {
 
     // MARK: - Initializers
 
+    /// Initializes a new operation token with a given cancellation closure.
+    /// - Parameter cancellation: A cancellation closure to be associeted with the token.
+    /// Called once after the token is cancelled
     init(cancellation: @escaping () -> Void) {
         self.cancellation = cancellation
     }
 
     // MARK: - Public methods
 
+    /// Cancels an associated operation by calling
+    /// an initially passed cancellation closure.
     public mutating func cancel() {
         isCancelled = true
     }
--- CocoaTypograf/Service/TypografService.swift
+++ CocoaTypograf/Service/TypografService.swift
@@ -8,12 +8,23 @@
 
 import Foundation
 
+/// A protocol describing a typograf service.
 public protocol TypografService {
 
+    /// A handler for an processing a result of processed text or a failure.
+    /// - Parameter result: A result of a text processing operation.
     typealias CompletionHandler = (_ result: Result<String, TypografServiceError>) -> Void
 
     // MARK: - Methods
 
+    /// Asynchronously performs a typographic processing of a given text with given parameters.
+    ///
+    /// After processing is completed the provided completion handler is called with a processing result.
+    ///
+    /// - Parameter text: A text to process.
+    /// - Parameter parameters: Parameters for processing the specified text.
+    /// - Parameter completion: A completion handler called after processing the text.
+    /// - Returns: An operation token that could be used for cancelling the asynchronous processing.
     @discardableResult
     func process(text: String,
                  parameters: ProcessTextParameters,
