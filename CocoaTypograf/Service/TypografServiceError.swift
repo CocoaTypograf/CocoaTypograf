a0d5ace2814eb96470306b2136b7e8b981fb8862
Vadim Zhilinkov <dreadct@gmail.com>
2020-08-12 21:32:03 +0700

    refs #30 Extend the TypografServiceError enum
    To conform to the CustomNSError protocol so it might expose underlying errors when required.

--- CocoaTypograf/Service/TypografServiceError.swift
+++ CocoaTypograf/Service/TypografServiceError.swift
@@ -8,8 +8,45 @@
 
 import Foundation
 
+/// A typograf service error.
 public enum TypografServiceError: Error {
-    case responseError(Error)
+
+    /// A typograf service response data is invalid.
     case invalidResponseData
+
+    /// A response error.
+    case responseError(Error)
+
+    /// A typograf service is unavailable for some reason.
     case serviceUnavailable
+
+}
+
+// MARK: - CustomNSError
+
+extension TypografServiceError: CustomNSError {
+
+    public var errorUserInfo: [String: Any] {
+        var result: [String: Any] = [:]
+        if let underlyingError = underlyingError {
+            result[NSUnderlyingErrorKey] = underlyingError
+        }
+        return result
+    }
+
+}
+
+// MARK: - Private methods and properties
+
+fileprivate extension TypografServiceError {
+
+    var underlyingError: Error? {
+        switch self {
+        case .responseError(let error):
+            return error
+        default:
+            return nil
+        }
+    }
+
 }
